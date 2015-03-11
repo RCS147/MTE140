74fa26068288941be538cfdd9530a30ba8f4ff4e
RCS147 <swiftryanc@gmail.com>
2015-03-11 09:17:49 -0400

    Finished CPP
    HPP v 1.0
    CPP v 1.0

--- Stack/Stack.cpp
+++ Stack/Stack.cpp
@@ -31,3 +31,42 @@ DataType Stack<DataType>::peek() const
 {
 	return top_ -> key;
 }
+
+template<class DataType>
+void Stack<DataType>::print(std::ostream & out) const
+{
+	if(empty())
+		out << "EMPTY_STACK" << std::endl;
+	else
+	{
+		Node* tmp = top_;
+		while(tmp != nullptr)
+		{
+			out << tmp -> key << std::endl;
+			tmp = tmp -> next;	
+		}
+	}
+}
+
+template<class DataType>
+void Stack<DataType>::push(DataType newKey)
+{
+	Node* newNode = new Node;
+	newNode -> next = top_;
+	newNode -> key = newKey;
+	top_ = newNode;
+}
+
+template<class DataType>
+DataType Stack<DataType>::pop()
+{
+	if(!empty())
+	{
+		Node* tmp = top_;
+		DataType value = tmp -> key;
+		top_ = top_ -> next;
+		
+		delete tmp;
+		return value;
+	}
+}
